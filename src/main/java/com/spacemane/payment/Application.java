e9ceb99af96f5cd0ad0cb7a3e7df24b714d43df8
Angelgonz <Angelgonz>
2021-10-05 05:30:47 +0000

    Initial commit

---
+++ src/main/java/com/spacemane/payment/Application.java
@@ -0,0 +1,10 @@
+package com.spacemane.payment;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
